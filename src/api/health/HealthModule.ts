1879babf648ace9db3ac65b7ad60e230ce7291e4
Nick A <nicolas.alonso@strapi.io>
2024-02-20 23:41:05 +0200

    filled load() method for health router

--- src/api/health/HealthModule.ts
+++ src/api/health/HealthModule.ts
@@ -1,9 +1,17 @@
+import Koa from "koa";
 import { IoCContainer } from "../../core/IoCContainer";
 import { HealthRouter } from "./HealthRouter";
+import Router from "@koa/router";
 const container: IoCContainer = IoCContainer.getInstance();
 
 export class HealthModule {
     public static register(): void {
         container.register(HealthRouter.name, new HealthRouter());
+    }
+
+    public static load(app: Koa): void {
+        const router: Router = container.resolve<HealthRouter>(HealthRouter.name).getRouter();
+        app.use(router.routes());
+        app.use(router.allowedMethods());
     }
 }
