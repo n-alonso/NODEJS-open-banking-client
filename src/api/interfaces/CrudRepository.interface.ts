d86ab151bc8b8ffba9f94628b62466c3a8dcb7bf
Nick A <nicolas.alonso@strapi.io>
2024-02-21 15:29:34 +0200

    refactored and deleted interfaces directory to group related content together

--- src/api/interfaces/CrudRepository.interface.ts
+++
@@ -1,7 +0,0 @@
-export interface CrudRepository<T> {
-    find(): Promise<T[]>;
-    findOne(id: string): Promise<T[]>;
-    // save(): Promise<T>;
-    // update(id: string): Promise<T>;
-    // remove(id: string): Promise<T>;
-}
